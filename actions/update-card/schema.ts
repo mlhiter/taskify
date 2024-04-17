90ce0eb75a1f111ed5950da6b37dff834f8fb32b
Alex Lee <3076438032@qq.com>
2024-04-17 20:05:25 +0800

    feat: card actions

--- actions/update-card/schema.ts
+++ actions/update-card/schema.ts
@@ -12,11 +12,13 @@ export const UpdateCard = z.object({
         message: 'Description must be at least 3 characters long',
       })
   ),
-  title: z
-    .string({
-      required_error: 'Title is required',
-      invalid_type_error: 'Title must be a string',
-    })
-    .min(3, 'Title must be at least 3 characters long'),
+  title: z.optional(
+    z
+      .string({
+        required_error: 'Title is required',
+        invalid_type_error: 'Title must be a string',
+      })
+      .min(3, 'Title must be at least 3 characters long')
+  ),
   id: z.string(),
 })
